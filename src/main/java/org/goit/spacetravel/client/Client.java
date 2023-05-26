cc4d938d7c2e05f24cdaeada535d26be4f04a27e
Denys Taranenko <sotar.channel@gmail.com>
2023-05-26 10:08:51 +0300

    HW 11 - v.1.1

--- src/main/java/org/goit/spacetravel/client/Client.java
+++ src/main/java/org/goit/spacetravel/client/Client.java
@@ -2,14 +2,23 @@ package org.goit.spacetravel.client;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
+import org.goit.spacetravel.ticket.Ticket;
+
+import java.util.List;
 
 @Entity
 @Data
 public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
+    @Column(name = "id")
     private int id;
 
     @Column
     private String name;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "client")
+    private List<Ticket> ticketList;
 }
--- src/main/java/org/goit/spacetravel/planet/Planet.java
+++ src/main/java/org/goit/spacetravel/planet/Planet.java
@@ -3,7 +3,12 @@ package org.goit.spacetravel.planet;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import lombok.ToString;
+import org.goit.spacetravel.ticket.Ticket;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -12,4 +17,12 @@ public class Planet {
     private String id;
     @Column
     private String name;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "planetFrom")
+    private List<Ticket> ticketsFromList;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "planetTo")
+    private List<Ticket> ticketsToList;
 }
